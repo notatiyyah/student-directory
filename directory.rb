44010e54b058af1438a80f4061739960f19e5f1f
AtiyyahRawat <atiyyah.rawat@gmail.com>
2020-12-01 14:01:21 +0000

    Add no. of students

---
+++ directory.rb
@@ -0,0 +1,23 @@
+puts "The students of Villains Academy"
+puts "-------------"
+# print banner
+
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+
+# print all students
+
+print "Overall, we have "
+print 11
+puts " great students"
+
+# print no. of students
