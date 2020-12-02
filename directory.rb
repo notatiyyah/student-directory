da29a36634b58111e242d822f62c24cb2ca57277
AtiyyahRawat <atiyyah.rawat@gmail.com>
2020-12-02 11:54:04 +0000

    Set up functions

--- directory.rb
+++ directory.rb
@@ -1,22 +1,33 @@
+def print_banner
+    puts "The students of Villains Academy\n-------------"
+    # print banner
+end
+
+def print_students(names)
+    names.each { |name| puts name }
+    #print all students
+end
+
+def print_footer(names)
+    puts "Overall, we have #{names.count} great students"
+    # print no. of students
+end
+
 students = [
- "Dr. Hannibal Lecter",
- "Darth Vader",
- "Nurse Ratched",
- "Michael Corleone",
- "Alex DeLarge",
- "The Wicked Witch of the West",
- "Terminator",
- "Freddy Krueger",
- "The Joker",
-"Joffrey Baratheon",
-"Norman Bates"
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
 ]
 # store all students in an array
-puts "The students of Villains Academy\n-------------"
-# print banner
-students.each { |student| puts student }
-# print all students
 
-print "Overall, we have #{students.count} great students"
-
-# print no. of students
+print_banner
+print_students(students)
+print_footer(students)
