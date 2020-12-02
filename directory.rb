f4a785b3bfc5af0d8ee3807a8ef8b8ebb0eee194
AtiyyahRawat <atiyyah.rawat@gmail.com>
2020-12-02 12:02:07 +0000

    Upgrade to using hashes to store student data

--- directory.rb
+++ directory.rb
@@ -3,8 +3,8 @@ def print_banner
     # print banner
 end
 
-def print_students(names)
-    names.each { |name| puts name }
+def print_students(students)
+    students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
     #print all students
 end
 
@@ -14,19 +14,19 @@ def print_footer(names)
 end
 
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
-# store all students in an array
+# store all students' info in an array
 
 print_banner
 print_students(students)
