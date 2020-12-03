3d2ca95e103f19b397502041deba50915131e6f4
AtiyyahRawat <atiyyah.rawat@gmail.com>
2020-12-03 11:28:11 +0000

    Add function to only print students whose names begin with a certain letter, also make all names title case

--- directory.rb
+++ directory.rb
@@ -4,7 +4,8 @@ def input_students
   # create an empty array</span>
   students = []
   # get the first name</span>
-  name = gets.chomp
+  name = gets.chomp.split.map(&:capitalize).join(' ')
+  # get the input, split it up per word, captialise them all, then join them again
   # while the name is not empty, repeat this code</span>
   while !name.empty?
     # add the student hash to the array</span>
@@ -24,9 +25,19 @@ end
 
 def print(students)
   students.each_with_index do |student,index| 
-    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
   #print all students
+end
+
+def print_students_beginning_with(students)
+  puts "Please enter the initial you want to search with"
+  letter = gets.chomp.upcase
+  students.each_with_index do |student,index|
+    if student[:name][0] == letter
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
 end
 
 def print_footer(names)
@@ -36,5 +47,5 @@ end
 
 students = input_students
 print_header
-print(students)
+print_students_beginning_with(students)
 print_footer(students)
